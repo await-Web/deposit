2744a4b05aba5fa270661ee566630cab78e93326
zengjunfeng <zengjunfeng@yinmaisoft.com>
2024-08-09 16:04:06 +0800

    修改登录

--- App.vue
+++ App.vue
@@ -1,4 +1,5 @@
 <script setup>
+	import 'dayjs/locale/zh-cn';
 	import {
 		onLaunch,
 		onShow,
@@ -7,33 +8,21 @@
 	import {
 		useUserStore
 	} from "@/store/user.js"
-	const userStore = useUserStore()
 	const uniIdCo = uniCloud.importObject('uni-id-co')
+	const userStore = useUserStore()
 	onLaunch(() => {
-		console.log('App onLaunch')
-		// 你课程没看完妈，我没有采用这种方式 开始是这种方式  这种不合理 那怎么改  改成我哪种独立登录页的，不要隐式登录，这种微信不运行允许好的 回去改  展示数据你用的是云函数嘛
-		let expired = uni.getStorageSync('uni_id_token_expired')
-		// if (expired - Date.now() > 1000 * 60 * 60 * 1) return
-		uni.login({
-			success: (res) => {
-				uniIdCo.loginByWeixin({
-					code: res.code
-				}).then(res => {
-					uni.removeStorageSync('uni-id-pages-userInfo')
-					let data = {
-						...res,
-						showToast: false
-					}
-					userStore.loginSuccess(data)
-				})
-			}
-		})
+		if ((uniCloud.getCurrentUserInfo().tokenExpired - Date.now()) < 1000 * 60 * 60 * 5) {
+			userStore.logout();
+		};
+
 	})
+
 	onShow(() => {
-		console.log('App onShow')
+		console.log('App Show')
 	})
+
 	onHide(() => {
-		console.log('App onHide')
+		console.log('App Hide')
 	})
 </script>
 
