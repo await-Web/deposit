8eaad9a703cc79bb3bfe7b2c3980dcfb36be677f
await-Web <20871209@qq.com>
2024-08-17 15:31:11 +0800

    去水印分享

--- pages/index/tool.vue
+++ pages/index/tool.vue
@@ -8,9 +8,9 @@
 			<view class="notice-bar-box">
 				<uni-notice-bar show-icon :scrollable="scrollable" text="一键粘贴用不了,就手动粘贴.有问题联系客服" color="red" />
 			</view>
-			<view class="u-m-t-20">
-				<uni-easyinput type="textarea" v-model="url" placeholder="此次粘贴视频分享链接"></uni-easyinput>
-				<button class="u-m-t-16" style="background-color: #16afc3;color: #fff;" @click="watermark">开始解析</button>
+			<view class="u-m-t-20 url-input">
+				<uni-easyinput type="textarea" v-model="url" placeholder="此处粘贴视频分享链接"></uni-easyinput>
+				<button class="u-m-t-16 btn" style="" @click="watermark">开始解析</button>
 			</view>
 			<view class="u-flex del-watermark">
 				<view class="course u-flex-col block">
@@ -20,6 +20,13 @@
 				<view class="invitation u-flex-col block">
 					<text class="txt-top">邀请好友</text>
 					<text class="txt-bottom">获取永久会员</text>
+				</view>
+			</view>
+			<view class="share u-flex-col" @click="share">
+				<view class="u-flex share-inner">
+					<image src="../../static/image/wx.png" mode=""></image>
+					<text class="u-m-l-10 u-m-r-10 u-font-30">分享给好友，共同解锁有趣的视频</text>
+					<button type="primary" size="mini">分享</button>
 				</view>
 			</view>
 			<view class="statement">视频归平台及作者所有，本应用不储存任何视频及图片</view>
@@ -37,6 +44,12 @@
 			return {
 				scrollable: true,
 				url: ''
+			}
+		},
+		onShareAppMessage() {
+			return {
+				title: '去水印',
+				path: '/pages/index/tool'
 			}
 		},
 		onLoad() {
@@ -81,8 +94,40 @@
 						}
 					});
 				})
-			}
+			},
+			share() {
 
+				// uni.showShareMenu({
+				// 	withShareTicket: true,
+				// 	menus: ['shareAppMessage', 'shareTimeline'],
+				// 	success: (res) => {
+				// 		console.log(res, '分享菜单显示成功');
+				// 	}
+				// });
+
+				// #ifdef MP-WEIXIN
+				wx.showShareMenu({
+					withShareTicket: true,
+					menus: ['shareAppMessage', 'shareTimeline']
+				})
+				// #endif
+
+
+
+				// uni.share({
+				// 	provider: "weixin",
+				// 	scene: "WXSceneSession",
+				// 	type: 1,
+				// 	summary: "我正在使用HBuilderX开发uni-app，赶紧跟我一起来体验！",
+				// 	success: function(res) {
+				// 		console.log("success:" + JSON.stringify(res));
+				// 	},
+				// 	fail: function(err) {
+				// 		console.log("fail:" + JSON.stringify(err));
+				// 	}
+				// });
+
+			}
 		}
 	}
 </script>
@@ -119,6 +164,13 @@
 
 			}
 
+			.url-input {
+				.btn {
+					background-color: #16afc3;
+					color: #fff;
+				}
+			}
+
 			.del-watermark {
 				width: 100%;
 				justify-content: space-between;
@@ -150,6 +202,26 @@
 				.invitation {
 					background-color: red;
 				}
+			}
+
+			.share {
+				.share-inner {
+					image {
+						width: 84rpx;
+						height: 84rpx;
+					}
+				}
+
+				width: 100%;
+				height: 180rpx;
+				margin-top: 20rpx;
+				border-radius: 20rpx;
+				background: radial-gradient(60% 200px at right top, #19b2bc, transparent),
+				radial-gradient(20% 200px at left top, #0a96e4, transparent),
+				radial-gradient(80% 200px at left top, #048af4, transparent);
+				color: #fff;
+				justify-content: center;
+				align-items: center;
 			}
 
 			.statement {
